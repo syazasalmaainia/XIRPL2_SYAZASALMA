fd58c5abced4aeafd07aa00b998890830e5f7783
kynenlarasingati <kynen.larasingati313@gmail.com>
2026-08-04 13:13:55 +0700

    Initial commit

---
+++ KKRPL_SYAZASALMA/XI_RPL_2_SYAZASALMA/HTML_DASAR/dasar_html.php
@@ -0,0 +1,27 @@
+<body>
+    <form>
+        nama:<input type="text" name="nama"> <br/>
+        alamat:<input type="text" name="alamat"> </br/>
+        password:<input type="password" name="pass"> <br/>
+        jenis kelamin:
+                <input type="radio" name="jk" value="laki-laki"/> Laki - Laki
+                <input type="radio" name="jk" value="perempuan"/> perempuan
+                <br/>
+        bahas pemograman yang dikuasai:
+                <input type="checkbox" name="web"> Web
+                <input type="checkbox" name="mobile"> MOBILE
+                <input type="checkbox" name="desk"> DESKTOP
+                <br/>
+        kota asal:
+            </select>
+                <option>Semarang</option>
+                <option>Bandung</option>
+            </select>
+        <br/>
+        pesan anda:
+        <textarea></textarea>
+        <br/>
+        <input type="submit" value="proses">
+    </form>
+</body>
+</html>
---
+++ KKRPL_SYAZASALMA/XI_RPL_2_SYAZASALMA/HTML_DASAR/pengenalan_html.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>
+            pengenalan HTML 
+        </title>
+     <head>
+    <body>
+        <h2 align="center">
+            WEB PENGENALAN HTML
+        <h2>
+    <body>
+<HTML> 
